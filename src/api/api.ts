5d72d841c02a6c6729e8e716efebd7c3e68c2e9f
diegofmsantos <diegofmsantos0@gmail.com>
2024-11-21 19:06:32 -0300

    ajuste api render

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import { Jogador } from '@/types/jogador'
 import axios, { AxiosResponse } from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://fabr-back.onrender.com',
+    baseURL: 'https://fabr-back.onrender.com/api',
 })
 
 // Função para buscar todos os times
