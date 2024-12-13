f357f65464959e104a865bf7ec507080e8c6b125
diegofmsantos <diegofmsantos0@gmail.com>
2024-12-12 21:57:28 -0300

    texto legenda ok

--- src/utils/statsExplanation.ts
+++ src/utils/statsExplanation.ts
@@ -1,72 +1,72 @@
 export const statsExplanations = {
     PASSE: {
         title: "As métricas de PASSE usadas pelo FABR Network",
-        description: "levam em consideração o número total de PASSES TENTADOS/quantidade de jogadores que foram alvos de passes na temporada.",
+        description: "levam em consideração a soma de todos os PASSES TENTADOS/NÚMERO DE JOGADORES que tiveram passes na temporada.",
         tiers: [
             "Tier 1: >= 40.9 (100% da média)",
             "Tier 2: >= 30.6 e < 40.9 (75% da média)",
             "Tier 3: < 30.6 (50% ou menos da média)"
         ],
-        totalPlayers: "1644 alvos/220 jogadores = 40.9"
+        totalPlayers: "1.882 passes tentados/46 jogadores = 40.9"
     },
     CORRIDA: {
         title: "As métricas de CORRIDA usadas pelo FABR Network",
-        description: "levam em consideração o número total de , quantidade de jogadores que foram na temporada.",
+        description: "levam em consideração a soma de todas as CARREGADAS/NÚMERO DE JOGADORES que tiveram carregadas na temporada.",
         tiers: [
             "Tier 1: >= 10.2 (100% da média)",
             "Tier 2: >= 7.6 e < 10.2 (75% da média)",
             "Tier 3: < 7.6 (50% ou menos da média)"
         ],
-        totalPlayers: "/220 jogadores = 10.2"
+        totalPlayers: "1.569 carregadas/154 jogadores = 10.2"
     },
     RECEPÇÃO: {
         title: "As métricas de RECEPÇÃO usadas pelo FABR Network",
-        description: "levam em consideração o número total de , quantidade de jogadores que foram na temporada.",
+        description: "levam em consideração a soma de todas os ALVOS DE RECEPÇÃO/NÚMERO DE JOGADORES que foram alvos na temporada.",
         tiers: [
             "Tier 1: >= 7.5 (100% da média)",
             "Tier 2: >= 5.6 e < 7.5 (75% da média)",
             "Tier 3: < 5.6 (50% ou menos da média)"
         ],
-        totalPlayers: "/220 jogadores = 7.5"
+        totalPlayers: "1.644 alvos de recepção/220 jogadores = 7.5"
     },
     RETORNO: {
         title: "As métricas de RETORNO usadas pelo FABR Network",
-        description: "levam em consideração o número total de , quantidade de jogadores que foram na temporada.",
+        description: "levam em consideração a soma de todos os RETORNOS/NÚMERO DE JOGADORES que tiveram retornos na temporada.",
         tiers: [
             "Tier 1: >= 2.9 (100% da média)",
             "Tier 2: >= 2.1 e < 2.9 (75% da média)",
             "Tier 3: < 2.1 (50% ou menos da média)"
         ],
-        totalPlayers: "/220 jogadores = 2.9"
+        totalPlayers: "267 retornos/93 jogadores = 2.9"
     },
     DEFESA: {
         title: "As métricas de DEFESA usadas pelo FABR Network",
-        description: "levam em consideração o número total de , quantidade de jogadores que foram na temporada.",
+        description: "levam em consideração a soma de todas as JOGADAS DEFENSIVAS/NÚMERO DE JOGADORES que tiveram jogadas defensivas na temporada.",
         tiers: [
             "Tier 1: >= 4.8 (100% da média)",
             "Tier 2: >= 3.6 e < 4.8 (75% da média)",
             "Tier 3: < 3.6 (50% ou menos da média)"
         ],
-        totalPlayers: "/220 jogadores = 4.8"
+        totalPlayers: "1.910 jogadas defensivas/401 jogadores = 4.8"
     },
     CHUTE: {
         title: "As métricas de CHUTE usadas pelo FABR Network",
-        description: "levam em consideração o número total de , quantidade de jogadores que foram na temporada.",
+        description: "levam em consideração a soma de todas os FIELD GOAL TENTADOS/NÚMERO DE JOGADORES que chutaram field gals tentados na temporada.",
         tiers: [
             "Tier 1: >= 1.8 (100% da média)",
             "Tier 2: >= 1.3 e < 1.8 (75% da média)",
             "Tier 3: < 1.3 (50% ou menos da média)"
         ],
-        totalPlayers: "/220 jogadores = 1.8"
+        totalPlayers: "54 field goals tentados/30 jogadores = 1.8"
     },
     PUNT: {
         title: "As métricas de PUNT usadas pelo FABR Network",
-        description: "levam em consideração o número total de , quantidade de jogadores que foram na temporada.",
+        description: "levam em consideração a soma de todos os PUNTS/NÚMERO DE JOGADORES que chutaram punts na temporada.",
         tiers: [
             "Tier 1: >= 5 (100% da média)",
             "Tier 2: >= 3.75 e < 5 (75% da média)",
             "Tier 3: < 3.75 (50% ou menos da média)"
         ],
-        totalPlayers: "/220 jogadores = 5"
-    },   
+        totalPlayers: "186 punts/37 jogadores = 5"
+    } 
 };
