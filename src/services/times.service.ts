4a66fa0ea263ad8c9a5661da70059888526fb45a
diegofmsantos <diegofmsantos0@gmail.com>
2025-07-15 19:53:53 -0300

    att chamadas de api

--- src/services/times.service.ts
+++ src/services/times.service.ts
@@ -4,12 +4,12 @@ import { BaseService } from './base.service'
 export class TimesService extends BaseService {
   static async getTimes(temporada: string = '2025'): Promise<Time[]> {
     const service = new TimesService()
-    return service.get<Time[]>(`/times/times`, { temporada })
+    return service.get<Time[]>(`/times`, { temporada })
   }
 
   static async getTime(id: number): Promise<Time> {
     const service = new TimesService()
-    return service.get<Time>(`times/time/${id}`)
+    return service.get<Time>(`/times/${id}`)
   }
 
   static async createTime(time: Omit<Time, 'id'>): Promise<Time> {
@@ -18,23 +18,22 @@ export class TimesService extends BaseService {
       ...time,
       temporada: time.temporada || '2025'
     }
-    return service.post<Time>('times/time', timeData)
+    return service.post<Time>('/times', timeData)
   }
 
   static async updateTime(id: number, time: Partial<Time>): Promise<Time> {
     const service = new TimesService()
-    return service.put<Time>(`times/time/${id}`, time)
+    return service.put<Time>(`/times/${id}`, time)
   }
 
   static async deleteTime(id: number): Promise<void> {
     const service = new TimesService()
-    return service.delete(`times/time/${id}`)
+    return service.delete(`/times/${id}`)
   }
 
   static async getTimeJogadores(timeId: number, temporada?: string): Promise<Jogador[]> {
     const service = new TimesService()
     const params = temporada ? { temporada } : {}
-    return service.get<Jogador[]>(`times/time/${timeId}/jogadores`, params)
+    return service.get<Jogador[]>(`/times/${timeId}/jogadores`, params)
   }
-
 }
