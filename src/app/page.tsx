1694cfa80997f97330986a44d774d7729c54c247
diegofmsantos <diegofmsantos0@gmail.com>
2025-08-16 15:28:43 -0300

    retirado ssr da page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,19 @@
 import { Lista } from "@/components/Lista"
 import { Metadata } from "next"
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
-import { queryKeys } from '@/hooks/queryKeys'
-import { TimesService } from '@/services/times.service'
 
 export const metadata: Metadata = {
   title: "Times",
 }
 
 export default async function Page() {
-  const queryClient = new QueryClient()
-  
-  if (process.env.NEXT_PUBLIC_API_BASE_URL) {
-    try {
-      await queryClient.prefetchQuery({
-        queryKey: queryKeys.times.list('2025'),
-        queryFn: () => TimesService.getTimes('2025')
-      })
-    } catch (error) {
-      console.log('Prefetch falhou, dados serão carregados no cliente')
-    }
-  }
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <div className="text-[#1414E] z-20 bg-[#ECECEC] lg:mr-40 xl:w-2/3 xl:mx-auto">
-        <h1 className="fixed inline-block w-full p-2 z-40 text-[53px] bg-[#ECECEC] text-[#272731] px-2 font-extrabold italic leading-[55px] pt-24 tracking-[-5px] 
+    <div className="text-[#1414E] z-20 bg-[#ECECEC] lg:mr-40 xl:w-2/3 xl:mx-auto">
+      <h1 className="fixed inline-block w-full p-2 z-40 text-[53px] bg-[#ECECEC] text-[#272731] px-2 font-extrabold italic leading-[55px] pt-24 tracking-[-5px] 
         lg:ml-32 xl:ml-64 xl:pt-10 2xl:ml-96">
-          ESCOLHA SEU TIME
-        </h1>
-        <Lista />
-      </div>
-    </HydrationBoundary>
+        ESCOLHA SEU TIME
+      </h1>
+      <Lista />
+    </div>
   )
 }
